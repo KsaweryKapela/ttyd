73a887c92b549c13222049ca7a367b5441b5c2a3
Simon Hryszko <simon@hryszko.dev>
2023-09-30 20:37:45 +0200

    start using docker compose

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker build -t ttyd . && docker run -p 80:80 ttyd
+[[ "$#" -ne 0 ]] && docker-compose down && exit 0
+
+docker build -t ttyd-api api #&& docker run -p 80:80 ttyd-api
+docker-compose up -d
 
 echo Api is running on:
 echo "http://localhost:80"

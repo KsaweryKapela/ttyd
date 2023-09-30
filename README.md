787539c1aa923f890da0e81e8c97dcaf42a40204
Simon Hryszko <simon@hryszko.dev>
2023-09-30 21:42:38 +0200

    fix

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ## Run api
 pip install -r requirements.txt  
 cd api  
-uvicorn main:app --host 0.0.0.0 --port 80  
+uvicorn main:app --host 0.0.0.0 --port 80
--- run.sh
+++ run.sh
@@ -2,9 +2,5 @@
 
 [[ "$#" -ne 0 ]] && docker-compose down && exit 0
 
-docker build -t ttyd-api api #&& docker run -p 80:80 ttyd-api
-docker-compose up -d
-
-echo Api is running on:
-echo "http://localhost:80"
-
+docker build -t ttyd-api api && docker run -p 80:80 ttyd-api
+# docker-compose up

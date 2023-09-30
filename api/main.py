745d53a85615ed85b1deb8bcca6a93cd625907ad
Simon Hryszko <simon@hryszko.dev>
2023-09-30 20:02:43 +0200

    update api

--- api/main.py
+++ api/main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 from common import *
-import os
-from dotenv import load_dotenv
 
 app = FastAPI()
-load_dotenv()
 
 @app.get("/")
 async def root():
-    return {"message": f"Hello World{os.environ.get('SQLITE_PATH')}"}
+    return {"message": f"Hello World"}
 
 @app.post("/query")
 async def data(q: str):

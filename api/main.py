fef7475816e130a0b54919f0c0e1eebb82d498d4
KsaweryKapela <ksawerykapela97@gmail.com>
2023-11-09 01:58:01 +0100

    Model works. Front end /backend needs fix

--- api/main.py
+++ api/main.py
@@ -4,10 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import torch
 import gc
-from transformers import AutoTokenizer, LlamaForCausalLM
+from transformers import AutoTokenizer, MistralForCausalLM, AutoModelForCausalLM, AutoConfig
 from common import *
 import os
 import re
+import sys
+import subprocess
+subprocess.check_call([sys.executable, "-m", "pip", "install", "peft"])
+from peft import AutoPeftModelForCausalLM
 
 try:
     from dotenv import load_dotenv
@@ -17,11 +21,15 @@ except ImportError:
 
 app = FastAPI()
 
-model_directory = "/home/ksaff/Desktop/ttyd/query_llama"
-tokenizer = AutoTokenizer.from_pretrained(model_directory)
-model = LlamaForCausalLM.from_pretrained(model_directory,
+gc.collect()
+torch.cuda.empty_cache()
+
+model_directory = '/opt/api/model'
+model = AutoPeftModelForCausalLM.from_pretrained(model_directory,
                                         load_in_8bit=True,
                                         device_map={'': 0})
+
+tokenizer = AutoTokenizer.from_pretrained(model_directory)
 
 origins = [
     "http://localhost:5173",
@@ -41,18 +49,18 @@ class QueryBody(BaseModel):
 
 @app.post("/query")
 async def data(body: QueryBody):
-    print(body)
+    try:
+        c.execute(body.query)
+        result = c.fetchall()
+        # Extract column names
+        columns = [desc[0] for desc in c.description]
+        
+        # Prepare the response
+        response = {"headers": columns, "data": result}
 
-    c.execute(body.query)
-    result = c.fetchall()
-
-    # Extract column names
-    columns = [desc[0] for desc in c.description]
-    
-    # Prepare the response
-    response = {"headers": columns, "data": result}
-
-    return response
+        return response
+    except:
+        print('query does not work')
 
 @app.get("/translate")
 async def translate(q: str):
@@ -65,20 +73,9 @@ class PromptBody(BaseModel):
     prompt: str
     query: str
 
-def infere_model(ddl, prompt, query):
-<<<<<<< HEAD
+def infere_model(ddl, prompt):
 
-    gc.collect()
-    torch.cuda.empty_cache()
-    model_directory = "mistralai/Mistral-7B-v0.1"
-    tokenizer = AutoTokenizer.from_pretrained(model_directory)
-    model = LlamaForCausalLM.from_pretrained(model_directory,
-                                            load_in_8bit=True,
-                                            device_map={'': 0})
-=======
-    gc.collect()
-    torch.cuda.empty_cache()
->>>>>>> 85cb5f729d06b9e9002eb4b44075deecc1e7fcf5
+
     prompt_2 = 'Make SQLite query based on DDL and instruction.'
     text = (
         prompt_2
@@ -88,33 +85,33 @@ def infere_model(ddl, prompt, query):
         + ddl
         + '### Response:\n'
     )
-
+    
     input_ids = tokenizer.encode(text, return_tensors='pt')
     input_ids = input_ids.to('cuda')
-    output = model.generate(input_ids, max_length=1000, temperature=0.3, num_return_sequences=1)
+    inputs = {
+    'input_ids': input_ids,
+    'max_length': 1000
+    }
+    output = model.generate(**inputs)
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    match = re.search(r'### Response:\s*(.*)', generated_text)
+    match = re.search(r'### Response:\s*(.*?)', generated_text)
     response = match.group(1)
+    print(generated_text)
     return response
 
 @app.post("/prompt")
 async def prompt(body: PromptBody):
     ddl = body.ddl
     prompt = body.prompt
-    query = body.query
-    print(body)
-<<<<<<< HEAD
-    model_response = infere_model(ddl, prompt, query)
+
+
+    model_response = infere_model(ddl, prompt)
 
     response = { 
         "query":
         model_response
     }
     return response
-=======
-    result = infere_model(ddl, prompt, query)
-    return result
->>>>>>> 85cb5f729d06b9e9002eb4b44075deecc1e7fcf5
 
 @app.get("/tables")
 async def get_tables():

84f909f4744fb4d1654032312e473bafa4f72af6
KsaweryKapela <ksawerykapela97@gmail.com>
2023-11-16 00:11:37 +0100

    Added ddl cleaner

--- api/model_handler.py
+++ api/model_handler.py
@@ -2,14 +2,31 @@ from transformers import AutoTokenizer
 import re
 from peft import AutoPeftModelForCausalLM
 
-
 model_directory = '/opt/api/model'
 model = AutoPeftModelForCausalLM.from_pretrained(model_directory,
                                         load_in_8bit=True,
                                         device_map={'': 0})
 tokenizer = AutoTokenizer.from_pretrained(model_directory)
 
+def clean_DDL(DDL):
+    lines = DDL.split('\n')
+    modified_text = "DDL: "
+    for line in lines:
+        if len(line) < 2:
+            pass
+        elif '/*' in line:
+            pass
+        elif 'DROP TABLE' in line:
+            pass
+
+        else:
+            line = re.sub(r'(?<!NOT )\bNULL\b', '', line)
+            modified_text += f'{line}\n'
+            
+    return modified_text
+
 def infere_model(ddl, prompt):
+    # ddl = clean_DDL(ddl)
     prompt_2 = 'Make SQLite query based on DDL and instruction.'
     text = (
         prompt_2

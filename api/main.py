9c7fd34c41516ad2fbf47d99514f5dbb1a2ddc86
KsaweryKapela <ksawerykapela97@gmail.com>
2024-01-17 22:29:43 +0100

    Created app

--- api/main.py
+++ api/main.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, request, jsonify, g
+from flask import Flask, render_template, request, jsonify, g, session
+from flask_session import Session
 import sqlite3
 
 
 app = Flask(__name__)
 
+app.secret_key = 'your_secret_key'  # Set this to a random string
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 DATABASE_PATH = '/home/ksaff/Desktop/sql/choosen_DB.sqlite'
 
 def get_db():
     db = sqlite3.connect(DATABASE_PATH)
     return db
+
+def reset_memory():
+    session.clear()  # This will clear the session
+    return '', 200
 
 @app.route('/')
 def index():
@@ -18,7 +26,17 @@ def index():
 def convert_query():
     data = request.get_json()
     user_query = data['user_query']
+
+    # Initialize or update session context
+    if 'context' not in session or len(session['context']) >= 3:
+        session['context'] = [user_query]
+    else:
+        session['context'].append(user_query)
+
+    # Use session['context'] to generate SQL query
+    # This is where you would use the sequence of inputs to generate the query
     sql_query = "SELECT EmployeeName FROM Salaries WHERE OvertimePay > 10000"
+
     return jsonify({'sql_query': sql_query})
 
 @app.route('/execute-query', methods=['POST'])

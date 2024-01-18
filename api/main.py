08ee8436ac4c9a44ac091c7d341cec1ac01f187c
KsaweryKapela <ksawerykapela97@gmail.com>
2024-01-19 00:45:10 +0100

    Implemented early model version into api

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, jsonify, g, session
 from flask_session import Session
 import sqlite3
-
+from model_handler import setup_conversation_buff, do_query, load_llm
 
 app = Flask(__name__)
 
-app.secret_key = 'your_secret_key'  # Set this to a random string
+app.secret_key = 'your_secret_key'
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 DATABASE_PATH = '/home/ksaff/Desktop/sql/choosen_DB.sqlite'
@@ -15,7 +15,8 @@ def get_db():
     return db
 
 def reset_memory():
-    session.clear()  # This will clear the session
+    session.clear()
+    del session['context']
     return '', 200
 
 @app.route('/')
@@ -27,15 +28,13 @@ def convert_query():
     data = request.get_json()
     user_query = data['user_query']
 
-    # Initialize or update session context
     if 'context' not in session or len(session['context']) >= 3:
         session['context'] = [user_query]
     else:
         session['context'].append(user_query)
+        
 
-    # Use session['context'] to generate SQL query
-    # This is where you would use the sequence of inputs to generate the query
-    sql_query = "SELECT EmployeeName FROM Salaries WHERE OvertimePay > 10000"
+    sql_query = do_query(question=user_query, conversation=conversation_buff)
 
     return jsonify({'sql_query': sql_query})
 
@@ -62,4 +61,6 @@ def execute_query():
         
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    llm = load_llm()
+    conversation_buff = setup_conversation_buff(llm)
+    app.run(debug=True, use_reloader=False)
--- api/model_handler.py
+++ api/model_handler.py
@@ -1,51 +1,91 @@
-from transformers import AutoTokenizer
-import re
-from peft import AutoPeftModelForCausalLM
+import torch
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from langchain import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, pipeline
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
 
-model_directory = '/opt/api/model'
-model = AutoPeftModelForCausalLM.from_pretrained(model_directory,
-                                        load_in_8bit=True,
-                                        device_map={'': 0})
-tokenizer = AutoTokenizer.from_pretrained(model_directory)
+def load_llm():
+    model_id = "codellama/CodeLlama-34b-hf"
 
-def clean_DDL(DDL):
-    lines = DDL.split('\n')
-    modified_text = "DDL: "
-    for line in lines:
-        if len(line) < 2:
-            pass
-        elif '/*' in line:
-            pass
-        elif 'DROP TABLE' in line:
-            pass
+    peft_model_id = "/home/ksaff/Desktop/ttyd/fine_tuning/2nd_try"
+    config = PeftConfig.from_pretrained(peft_model_id)
 
-        else:
-            line = re.sub(r'(?<!NOT )\bNULL\b', '', line)
-            modified_text += f'{line}\n'
-            
-    return modified_text
+    bnb_config = BitsAndBytesConfig(
+                                    load_in_4bit=True,
+                                    bnb_4bit_use_double_quant=True,
+                                    bnb_4bit_quant_type="nf4",
+                                    bnb_4bit_compute_dtype=torch.bfloat16
+                                    )
 
-def infere_model(ddl, prompt):
-    # ddl = clean_DDL(ddl)
-    prompt_2 = 'Make SQLite query based on DDL and instruction.'
-    text = (
-        prompt_2
-        + '### Instruction:\n'
-        + prompt
-        + '### Input:\n'
-        + ddl
-        + '### Response:\n'
+    model = AutoModelForCausalLM.from_pretrained(
+        config.base_model_name_or_path,
+        return_dict=True,
+        quantization_config=bnb_config, device_map={'': 0}
     )
+    tokenizer = AutoTokenizer.from_pretrained(
+                                            config.base_model_name_or_path,
+                                            )
 
-    input_ids = tokenizer.encode(text, return_tensors='pt')
-    input_ids = input_ids.to('cuda')
-    inputs = {
-    'input_ids': input_ids,
-    'max_length': 1000
-    }
-    output = model.generate(**inputs)
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    match = re.search(r"### Response:\s*(.+?);", generated_text)
-    response = match.group(1)
+    generation_config = GenerationConfig.from_pretrained(model_id)
+    generation_config.max_new_tokens = 1024
+    generation_config.max_time = 3
+    generation_config.pad_token_id = 2
+    
+    text_pipeline = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        generation_config=generation_config,
+    )
+    
+    llm = HuggingFacePipeline(pipeline=text_pipeline)
+    return llm
+
+def create_prompt(question):
+    database_scheme = """
+                         CREATE TABLE Salaries
+                         Id INTEGER PRIMARY KEY, -- Unique ID for each employee
+                         EmployeeName VARCHAR, -- Name of the employee
+                         JobTitle VARCHAR, -- Name of employees proffesion
+                         BasePay NUMERIC, -- Base pay of employee
+                         OvertimePay NUMERIC, -- Overtime pay of employee
+                         OtherPay NUMERIC, -- Other pays of employee
+                         Benefits NUMERIC, -- Benefits of employee
+                         TotalPay NUMERIC, -- Total pay of employee
+                         TotalPayBenefits NUMERIC, -- Sum of pay benefits of employee
+                         Year INTEGER, -- Year data from row reffers to
+                      """
+    text = (
+            f"""
+            ### Task
+            Generate a SQL query to answer the following question:
+            {question}
+            ### Database Schema
+            This query will run on a database whose schema is represented in this string:""" 
+            +
+            database_scheme
+            +
+            f"""
+            ### SQL
+            Given the database schema, here is the SQL query that answers `{question}`:
+            ```sql
+            """
+    )
+    return text
+
+def setup_conversation_buff(llm):
+    memory = ConversationBufferMemory()
+    conversation_buf = ConversationChain(
+        llm=llm,
+        memory=memory)
+    return conversation_buf
+
+def do_query(question, conversation):
+    text = create_prompt(question)
+    response = conversation(text)
+    print(f'question - {question}')
     print(response)
-    return response
+    raw_query = response['response'].split("```")[-2].replace('sql', '')
+    return response['response']
